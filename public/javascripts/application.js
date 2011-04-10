df2b7e4e25bcc3db769ebc195aad328538e7af63
Leonardo Borges <leonardoborges.rj@gmail.com>
2011-04-10 14:14:39 +1000

    added feature detection for the speech api

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,11 @@
 var voiceCommands;
 
+function speechcapable(){
+    var elem = document.createElement('input');
+    var support = 'onwebkitspeechchange' in elem || 'speech' in elem;
+    return support;
+}
+
 $(function(){
   var twitter = LoudParrot.Twitter(jQuery, {
     resultsContainer:'#result',
@@ -10,4 +16,8 @@ $(function(){
   $('#tweet-link').click(function(){
     twitter.postToBackEnd($('#tweet-text').val());
   });
+
+  if(!speechcapable()){
+    $('body').html('Your browser does not support speech recognition. Try one the Google Chrome Beta Channel: <a href="http://www.google.com/intl/en/landing/chrome/beta/">http://www.google.com/intl/en/landing/chrome/beta/</a>');
+  }
 });
